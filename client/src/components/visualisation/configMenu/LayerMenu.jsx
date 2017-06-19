3d050445f62c652177f77e9411f21e82ea33e363
Loïc Sans <loic.sans@gmail.com>
2017-06-19 15:48:07 +0300

    [#804] Fix build

--- client/src/components/visualisation/configMenu/LayerMenu.jsx
+++ client/src/components/visualisation/configMenu/LayerMenu.jsx
@@ -24,17 +24,11 @@ export default class LayerMenu extends Component {
 
     return (
       <div className="LayerMenu">
-        <div
-          className="controlRow"
-        >
-          <span
-            className="titleContainer"
-          >
+        <div className="controlRow">
+          <span className="titleContainer">
             <h3>Layers</h3>
           </span>
-          <span
-            className="buttonContainer"
-          >
+          <span className="buttonContainer">
             <button
               className="addLayer clickable noSelect"
               onClick={onAddLayer}
