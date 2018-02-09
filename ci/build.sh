8231031cc2dea521af3b8e4c3995eccd44588a63
Dan Lebrero <dlebrero@gmail.com>
2018-02-09 10:19:24 +0100

    [#1270] Logging to gcloud to be able to pull images

--- ci/build.sh
+++ ci/build.sh
@@ -11,6 +11,19 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
+log Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
+gcloud config set project akvo-lumen
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
+
 log Starting Docker Compose environment
 docker-compose -p akvo-lumen-ci -f docker-compose.yml -f docker-compose.ci.yml up --no-color -d --build
 
