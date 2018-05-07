bd63cbd7f9f0562144d039a3c394f2d78a4fff5a
Juan A. Ruz <juanantonioruz@gmail.com>
2018-05-07 12:50:22 +0200

    Update multiple rows in one sql call

--- backend/src/akvo/lumen/transformation/derive.sql
+++ backend/src/akvo/lumen/transformation/derive.sql
@@ -7,6 +7,25 @@ SELECT * FROM :i:table-name
 -- :doc Set cell value
 UPDATE :i:table-name SET :i:column-name = :value WHERE rnum=:rnum
 
+-- :name set-cells-value :!
+-- :doc set multiple cell values
+/* :require [clojure.string :as string] */
+UPDATE :i:table-name 
+SET :i:column-name = c.derived_val:::i:column-type
+FROM (values
+/*~
+;; we need to cover 3 types => dates, numbers and strings (and nil)
+(->> (:params params)
+     (map (fn [[i v]]
+            (format (cond
+                      (inst? v) "(%s, '%s')" 
+                      (string? v) "(%s, '%s')" 
+                      :else "(%s, %s)") i v)))
+     (string/join ","))
+~*/
+) as c(ref_num, derived_val)
+WHERE rnum=c.ref_num;
+
 -- :name delete-row :!
 -- :doc Delete a row
 DELETE FROM :i:table-name WHERE rnum=:rnum
